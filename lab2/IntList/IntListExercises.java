6ee4c2cced787e54b38ea262fbc904405dc0d4d7
sink-sea <iwaxi601@gmail.com>
2024-08-08 00:04:37 +0800

    Finished Lab2.

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -3,21 +3,21 @@ package IntList;
 public class IntListExercises {
 
     /**
-     * Part A: (Buggy) mutative method that adds a constant C to each
+     * Part A: Mutative method that adds a constant C to each
      * element of an IntList
      *
      * @param lst IntList from Lecture
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
     }
 
     /**
-     * Part B: Buggy method that sets node.first to zero if
+     * Part B: Method that sets node.first to zero if
      * the max value in the list starting at node has the same
      * first and last digit, for every node in L
      *
@@ -51,7 +51,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -59,7 +59,7 @@ public class IntListExercises {
     }
 
     /**
-     * Part C: (Buggy) mutative method that squares each prime
+     * Part C: Mutative method that squares each prime
      * element of the IntList.
      *
      * @param lst IntList from Lecture
@@ -76,7 +76,7 @@ public class IntListExercises {
         if (currElemIsPrime) {
             lst.first *= lst.first;
         }
-
-        return currElemIsPrime || squarePrimes(lst.rest);
+        boolean nxtElemIsPrime = squarePrimes(lst.rest);
+        return currElemIsPrime || nxtElemIsPrime;
     }
 }
