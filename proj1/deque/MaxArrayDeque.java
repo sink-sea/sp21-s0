9b0f3f46c05b752a24501bc473346cfac424007f
sink-sea <iwaxi601@gmail.com>
2024-08-14 11:34:29 +0800

    Add max-array-deque.

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,37 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T>{
+    Comparator<T> compare;
+    public MaxArrayDeque(Comparator<T> c) {
+        compare = c;
+    }
+
+    public T max() {
+        if (isEmpty()) {
+            return null;
+        }
+        T max = this.get(0);
+        for (T val : this) {
+            if (compare.compare(val, max) > 0) {
+                max = val;
+            }
+        }
+        return max;
+    }
+
+    public T max(Comparator<T> c) {
+        if (isEmpty()) {
+            return null;
+        }
+        T max = this.get(0);
+        for (T val : this) {
+            if (c.compare(val, max) > 0) {
+                max = val;
+            }
+        }
+        return max;
+    }
+
+}
